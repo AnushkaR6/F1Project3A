38d640e6f39914334cbf3662aecdf88bef0b3a66
AnushkaR6 <91567074+AnushkaR6@users.noreply.github.com>
2026-03-28 11:59:38 -0700

    Supabase updates

--- Project3/Model/Service/AuthServiceService.swift
+++ Project3/Model/Service/AuthServiceService.swift
@@ -1,7 +1,18 @@
 //
-//  SupabaseService.swift
+//  AuthService.swift
 //  Project3
 //
 //  Created by Anushka R on 3/28/26.
 //
 
+import Foundation
+import Supabase
+
+class AuthService {
+    func start() {
+        let client = SupabaseClient(
+            supabaseURL: URL(string: "https://xyzcompany.supabase.co")!,
+            supabaseKey: "public-anon-key"
+        )
+    }
+}
